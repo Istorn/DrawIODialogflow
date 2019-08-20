036b3b643b254d6dfd3e3074d657228ade61abe9
Lorenzo Neri (Istorn) <istorn.one@gmail.com>
2019-08-20 21:17:07 +0200

    Individuazione parameters

--- parserGraph.js
+++ parserGraph.js
@@ -50,11 +50,13 @@ module.exports={
                             //Nome
                             var name=valueXML.substr(valueXML.indexOf("nome: ")+"nome: ".length,valueXML.indexOf("<br>")-"nome: ".length);
                             valueXML=valueXML.substr(valueXML.indexOf("<br>")+4,valueXML.length);
-                            if (valueXML.indexOf("parameters: ")>0){
+                            if (valueXML.indexOf("parameters: ")>=0){
                                 //Ci sono variabili di input da prendere
                                 var parameters=[];
                                 var stringParameters=valueXML.substr(valueXML.indexOf("parameters: ")+"parameters: ".length,valueXML.indexOf("<br>")-"parameters: ".length);
-                                parameters=stringParameters.split("#");
+                                stringParameters.split(",").forEach(element => {
+                                    parameters.push(getParameterByXML(element));
+                                });
                                 valueXML=valueXML.substr(valueXML.indexOf("<br>")+4,valueXML.length);
                                 
                                 
@@ -62,7 +64,7 @@ module.exports={
                             }
                             var phrases=[];
                             var stringPhrases=valueXML.substr(valueXML.indexOf("phrases: ")+"phrases: ".length,valueXML.indexOf("<br>")-"phrases: ".length);
-                            phrases=stringPhrases.split("#");
+                            phrases=stringPhrases.split(",");
                             //Per ogni frase, dobbiamo andare a prendere il parametro
                             //Che poi diventeranno i parametri in base al colore
                             //
@@ -96,3 +98,39 @@ module.exports={
     });
     return agentXML;
 }}
+
+//Identifichiamo i parametri inseriti nella training phrase
+function splitTrainingPhrases(phrase,parameters){
+    //In base a come sono state colorate le parole tipo nella frase, corrispondono ai corrispettivi parametri identificati in precedenza
+
+}
+
+//Impostiamo i parametri in base ai colori e ai nomi
+function getParameterByXML(parameterString){
+    var color=parameterString.substr(parameterString.indexOf("color=\"#")+"color=\"#".length+1,parameterString.indexOf("\">")+"\">".length-"color=\"#".length);
+    color=color.substr(0,color.indexOf("\">"));
+    var name=parameterString.substr(parameterString.indexOf("\">")+"\">".length,parameterString.indexOf("</font>")-8);
+    name=name.substr(0,name.indexOf("</font>"));
+    return {
+        color: color,
+        name: name
+    }
+}
+
+
+//Restituisce i termini chiave nella frase in funzione del colore del parametro da impostare nell'agente
+function findParameterInPhraseByColor(phrase,parameterColor){
+    var keyTerms=[];
+    if (phrase.match(/<font/g).length>0){
+        //Abbiamo almeno un termine chiave
+        var colorOfTerm=color=phrase.substr(phrase.indexOf("color=\"#")+"color=\"#".length+1,phrase.indexOf("\">")+"\">".length-"color=\"#".length);
+        colorOfTerm=substr(0,colorOfTerm.indexOf("\">"));
+        if (colorOfTerm==parameterColor){
+            //È un termine chiave 
+            var keyTerm=phrase.substr(phrase.indexOf("\">")+"\">".length,phrase.indexOf("</font>")-8);
+            keyTerm=keyTerm.substr(0,name.indexOf("</font>"));
+            keyTerms.push(keyTerm);
+        }
+    }
+    return keyTerms;
+}
